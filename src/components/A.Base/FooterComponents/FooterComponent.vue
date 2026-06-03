0963e07d217b90209e2ef374db8cec768f830834
BerDavoglio <berdavoglio@gmail.com>
2026-06-02 22:02:06 -0300

    feat: created component for footer

--- src/components/A.Base/FooterComponents/FooterComponent.vue
+++ src/components/A.Base/FooterComponents/FooterComponent.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="flex justify-between w-[75vw] m-auto">
-    <div>Logo</div>
-    <div>Links</div>
-    <div>Contato</div>
-    <div>Termos</div>
+  <div class="w-[40vw] m-auto pb-4">
+    <div class="justify-center">
+      <img src="@/assets/Logo.png" class="w-50" />
+      <div>Organize demandas e acompanhe o trabalho da sua equipe.</div>
+    </div>
+    <div class="border border-primary my-3" />
+    <div class="columns-2 justify-between m-auto my-8 gap-8">
+      <div class="grid columns-1 justify-items-end">
+        <div class="mb-2 font-bold text-lg">Produto</div>
+        <FooterMenuItem text="Recursos" />
+        <FooterMenuItem text="Como funciona" />
+        <FooterMenuItem text="Login" />
+        <FooterMenuItem text="Criar conta" />
+      </div>
+      <div>
+        <div class="mb-2 font-bold text-lg">Empresa</div>
+        <FooterMenuItem text="Sobre" />
+        <FooterMenuItem text="Contato" />
+        <FooterMenuItem text="Privacidade" />
+        <FooterMenuItem text="Termos" />
+      </div>
+    </div>
+    <div class="border border-primary my-3" />
+    <div class="text-sm">© 2026 FlowDesk. Todos os direitos reservados. </div>
   </div>
 </template>
 
 <script setup>
+import FooterMenuItem from './FooterMenuItem.vue';
+
 </script>
---
+++ src/components/A.Base/FooterComponents/FooterMenuItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hover:border-b cursor-pointer w-fit">
+    {{ text }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  text: String,
+})
+</script>
